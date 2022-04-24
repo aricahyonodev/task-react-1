ab8b4490e1f04795a52e9eda999f9e443c249fcf
Ari Cahyono <cahyono.ari81@gmail.com>
2022-04-24 13:43:55 +0700

    Add: Not Found Page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { Home } from './page/Home';
 import { ProductList } from './page/ProductList';
 import { ProducDetail } from './page/ProducDetail';
+import { NotFound } from './page/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
-        <Link to="/">Home</Link> | 
-        <Link to="product-list">Product List</Link> |
+        <Link to="/">Home</Link> |<Link to="product-list">Product List</Link> |
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product-list" element={<ProductList />} />
         <Route path="/product-detail/:id" element={<ProducDetail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
